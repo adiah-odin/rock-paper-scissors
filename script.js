6243f774906b47faa7a4d998c1194d13fc94970e
nathanieladiah <nathanieladiah@gmail.com>
2022-05-16 19:18:22 -0400

    Create function to compare player and computer

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-const playOptions = ['Rock', 'Paper', 'Scisors'];
+const playOptions = ['Rock', 'Paper', 'Scissors'];
 
 function computerPlay() {
 	// Should choose randomly from rock, paper, scissors.
@@ -6,8 +6,50 @@ function computerPlay() {
 }
 
 function choose(choices) {
+	// chooses a random option from a list of options
 	let index = Math.floor(Math.random() * choices.length);
 	return choices[index];
 }
 
-console.log(computerPlay());
+function playRound(playerSelection, computerSelection){
+	// Declares a winner based on playerSelection and computerSelection
+	switch (playerSelection) {
+		case 'rock':
+			switch (computerSelection) {
+				case 'rock':
+					return "It's a tie!";
+				case 'paper': 
+					return "You lose! Paper beats Rock";
+				case 'scissors':
+					return "You win! Rock beats Scissors";
+				default:
+					return "Something went wrong";
+			};
+		case 'paper':
+			switch (computerSelection) {
+				case 'rock':
+					return "You win! Paper beats Rock";
+				case 'paper': 
+					return "It's a tie!";
+				case 'scissors':
+					return "You lose! Scissors beats Paper";
+				default:
+					return "Something went wrong";
+			};
+		case 'scissors':
+			switch (computerSelection) {
+				case 'rock':
+					return "You lose! Rock beats Scissors";
+				case 'paper': 
+					return "You win! Scissors beats Paper";
+				case 'scissors':
+					return "It's a tie!";
+				default:
+					return "Something went wrong";
+			};
+	}
+}
+
+
+
+console.log(playRound('rock', computerPlay().toLowerCase()));
