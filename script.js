4dd1693e56d078b03f791073d5ff1b4c19983789
nathanieladiah <nathanieladiah@gmail.com>
2022-05-16 20:57:40 -0400

    Create function to run 5 rounds

--- script.js
+++ script.js
@@ -13,7 +13,7 @@ function choose(choices) {
 
 function playRound(playerSelection, computerSelection){
 	// Declares a winner based on playerSelection and computerSelection
-	switch (playerSelection) {
+	switch (playerSelection.toLowerCase()) {
 		case 'rock':
 			switch (computerSelection) {
 				case 'rock':
@@ -62,5 +62,35 @@ function getPlayerChoice() {
 	return choice;
 }
 
-console.log(playRound(getPlayerChoice(), computerPlay().toLowerCase()));
+function game() {
+	// Play five rounds and keep score
+	let computerScore = 0;
+	let playerScore = 0;
+
+	for (let i = 0; i< 5; i++) {
+		// console.log(playRound(getPlayerChoice(), computerPlay()));
+		let result = playRound(getPlayerChoice(), computerPlay());
+		console.log(result);
+		if (result.includes('win')) {
+			playerScore++;
+		} else if (result.includes('lose')) {
+			computerScore++;
+		}
+	}
+
+	if (playerScore > computerScore) {
+		console.log('Player Wins');
+	} else if (computerScore > playerScore) {
+		console.log('Computer wins');
+	} else {
+		console.log("Game tie");
+	}
+
+	console.log(`Computer score: ${computerScore}`);
+	console.log(`Player Score: ${playerScore}`);
+
+}
+
+game();
+
 // getPlayerChoice();
